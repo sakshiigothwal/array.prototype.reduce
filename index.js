a9a1fe97184d12012ee149cce9d481d8a99fa109
sakshiigothwal <119726876+sakshiigothwal@users.noreply.github.com>
2025-06-02 14:59:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+if (!Array.prototype.reduce) {
+  Array.prototype.reduce = function(cb, initialValue) {
+    let acc = initialValue;
+    let start = 0;
+
+    for (let i = start; i < this.length; i++) {
+      acc = cb(a, this[i], i, this);
+    }
+
+    return acc;
+  };
+}
+const arr = [4, 6, 19, 20, 22];
+
+const sum = arr.reduce((acc, curr) => acc + curr, 0);
+console.log("Sum:",sum); 
+
